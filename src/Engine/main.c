16a756eb9250a2e5a217d83db578bfe0f6ee7f3c
muntasirfuad <muntasirfuad22@gmail.com>
2026-07-13 00:10:32 +0600

    Update main.c

--- src/Engine/main.c
+++ src/Engine/main.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
-int main()
+#include "raylib.h"
+#include "game.h"
+
+int main(void)
 {
-    printf("In IUT CGPA matters");
+    InitWindow(1280, 720, "Chronicles of the Fallen Hero");
+    SetTargetFPS(60);
+
+    InitGame();
+
+    while (!WindowShouldClose())
+    {
+        UpdateGame();
+
+        BeginDrawing();
+        DrawGame();
+        EndDrawing();
+    }
+
+    CloseGame();
+    CloseWindow();
     return 0;
 }
