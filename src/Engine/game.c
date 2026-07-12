d2086b3c501f51e1ee79853a9ca5d2fbc1d81aef
muntasirfuad <muntasirfuad22@gmail.com>
2026-07-13 00:09:36 +0600

    Update game.c

---
+++ src/Engine/game.c
@@ -0,0 +1,48 @@
+#include "raylib.h"
+#include "dialogue.h"
+#include "scene_ch1_forest.h"
+
+typedef enum{
+    STATE_PROLOGUE,
+    STATE_CHAPTER1_FOREST
+}GameState;
+
+static GameState state;
+
+void InitGame(void){
+    state = STATE_PROLOGUE;
+    InitDialogue();
+}
+
+void UpdateGame(void){
+    switch(state){
+        case STATE_PROLOGUE:
+            UpdateDialogue();
+            if(IsDialogueFinished()){
+                CloseDialogue();
+                InitDialogueCh1();
+            }
+            break;
+
+        case STATE_CHAPTER1_FOREST:
+            UpdateDialogueCh1();
+            break;
+    }
+}
+
+void DrawGame(void){
+    switch(state){
+        case STATE_PROLOGUE:
+            DrawDialogueBox();
+            break;
+
+        case STATE_CHAPTER1_FOREST:
+            DrawDialogueBoxCh1();
+            break;
+    }
+}
+
+void CloseGame(void){
+    if(state == STATE_PROLOGUE) CloseDialogue();
+    else CloseDialogueCh1();
+}
