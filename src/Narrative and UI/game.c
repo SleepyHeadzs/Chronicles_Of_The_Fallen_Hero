52afcfd05ed02e51260500aca25068a4bd5d37ae
muntasirfuad <muntasirfuad22@gmail.com>
2026-07-13 00:08:41 +0600

    Add files via upload

---
+++ src/Narrative and UI/game.c
@@ -0,0 +1,49 @@
+#include "raylib.h"
+#include "dialogue.h"
+#include "scene_ch1_forest.h"
+
+typedef enum{
+    STATE_PROLOGUE,
+    STATE_CHAPTER1_FOREST
+}GameState;
+
+static GameState state;
+
+void InitGame(void){
+    state = STATE_PROLOGUE;
+    InitDialogue();              // prologue shuru
+}
+
+void UpdateGame(void){
+    switch(state){
+        case STATE_PROLOGUE:
+            UpdateDialogue();
+            if(IsDialogueFinished()){
+                CloseDialogue();
+                InitDialogueCh1();        // <- Chapter 1 shuru, actual naam onujayi
+                state = STATE_CHAPTER1_FOREST;
+            }
+            break;
+
+        case STATE_CHAPTER1_FOREST:
+            UpdateDialogueCh1();          // <- actual naam
+            break;
+    }
+}
+
+void DrawGame(void){
+    switch(state){
+        case STATE_PROLOGUE:
+            DrawDialogueBox();
+            break;
+
+        case STATE_CHAPTER1_FOREST:
+            DrawDialogueBoxCh1();         // <- actual naam
+            break;
+    }
+}
+
+void CloseGame(void){
+    if(state == STATE_PROLOGUE) CloseDialogue();
+    else CloseDialogueCh1();              // <- actual naam
+}
