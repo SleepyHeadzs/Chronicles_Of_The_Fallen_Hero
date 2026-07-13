b0127b3943d3243c4cf19ae568d8671f1af4952f
Cryphion <sabinahmed@protonmail.com>
2026-07-14 05:30:31 +0600

    game.h by Sabin

--- src/Engine/game.h
+++ src/Engine/game.h
@@ -1,9 +1,24 @@
 #ifndef GAME_H
 #define GAME_H
 
-void InitGame(void);
-void UpdateGame(void);
-void DrawGame(void);
-void CloseGame(void);
+#include <stdbool.h>
+
+typedef enum {
+    STATE_PROLOGUE,
+    STATE_CH1_FOREST,
+    STATE_COUNT
+} GameState;
+
+typedef struct {
+    GameState current;
+    GameState next;
+    bool transitioning;
+    float fadeAlpha;
+} GameContext;
+
+void Game_Init(GameContext *ctx);
+void Game_Update(GameContext *ctx, float dt);
+void Game_Draw(GameContext *ctx);
+void Game_RequestTransition(GameContext *ctx, GameState next);
 
 #endif
