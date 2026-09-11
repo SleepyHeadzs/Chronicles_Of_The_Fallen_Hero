e681190225ee3f419bf042e662c55faa314bead3
Sakib Al Hasan <sakibalhasan212715@gmail.com>
2026-09-12 01:19:15 +0600

    add struct

--- src/puzzle_system.c
+++ src/puzzle_system.c
@@ -1,3 +1,9 @@
 #include "raylib.h"
 #include "puzzle_system.h"
 #include <string.h>
+
+typedef struct{
+    const char *question;
+    const char *options[4];
+    int correctAnswer;
+}QuizQuestion;
