bb90655f1a548bc58a0432f559ce92784ed2f095
Cryphion <sabinahmed@protonmail.com>
2026-09-14 08:11:12 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
 #include "raylib.h"
 #include "game.h"
 
-int main()
+bool ShouldGameQuit(void);
+
+int main(void)
 {
     InitWindow(1280, 720, "Chronicles of the Fallen Hero");
+    if (!IsWindowReady())
+        return 1;
     SetTargetFPS(60);
-
+    InitAudioDevice();
     InitGame();
 
     while (!WindowShouldClose())
     {
         UpdateGame();
+        if (ShouldGameQuit())
+            break;
 
         BeginDrawing();
         ClearBackground(BLACK);
@@ -19,6 +25,8 @@ int main()
     }
 
     CloseGame();
+    if (IsAudioDeviceReady())
+        CloseAudioDevice();
     CloseWindow();
     return 0;
 }
